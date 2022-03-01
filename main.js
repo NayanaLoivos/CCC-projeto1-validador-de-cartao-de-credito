696964e85a252452cc9ca4faada35cdefdd67263
Nayana Loivos <loivosdev@gmail.com>
2022-03-01 15:49:07 -0300

    Obtendo array de index par menor que 10

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
     let digitosDoCartao = document.querySelector('#boxInputDigitosCartao');
     let arrayInversaDigitosDoCartao = [];
     let arrayPosicaoParDaArrayInversaDigitosDoCartao = [];
+    let arrayPosicaoParMenor10 = [];
+    let arrayPosicaoImparDaArrayInversaDigitosDoCartao = [];
     let arrayDeNumerosMaioresQue9DaArrayPosicaoParDaArrayInversaDigitosDoCartao = [];
-    let concatenandoOsNovosDigitosDocartao = [];
+    let concatenandoOsNovosDigitosDocartao;
 
 
     function exibirDigitosDoCartao () {
@@ -23,12 +25,28 @@
 
     function obterArrayIndexParVezes2 () {
         for(let i = 0; i < arrayInversaDigitosDoCartao.length; i++) {
-            if( i % 2 === 0 && i !== 0) {
+            if (i % 2 === 0 && i !== 0) {
                 let posicao = arrayInversaDigitosDoCartao[i];
-                arrayPosicaoParDaArrayInversaDigitosDoCartao.push(posicao*(2));
+                arrayPosicaoParDaArrayInversaDigitosDoCartao.push(posicao * (2));
+
+            }else {
+                let posicao = arrayInversaDigitosDoCartao[i];
+                arrayPosicaoImparDaArrayInversaDigitosDoCartao.push(posicao);
             }
+
         }
         console.log(arrayPosicaoParDaArrayInversaDigitosDoCartao); //testar
+        console.log(arrayPosicaoImparDaArrayInversaDigitosDoCartao); //testar        c
+    }
+
+    function arrayPosicaoParMenorQue10 () {
+        for(let i =0; i < arrayPosicaoParDaArrayInversaDigitosDoCartao.length; i++) {
+            if(arrayPosicaoParDaArrayInversaDigitosDoCartao[i] < 10) {
+                let posicao = arrayPosicaoParDaArrayInversaDigitosDoCartao[i];
+                arrayPosicaoParMenor10.push(posicao);
+            }
+        }
+        console.log(arrayPosicaoParMenor10);
     }
 
     function obterArrayDeIndexParVezes2MaioresQue9 () {
@@ -48,6 +66,7 @@
             exibirDigitosDoCartao();
             obterArrayInversaDoNumeroDoCartao();
             obterArrayIndexParVezes2();
+            arrayPosicaoParMenorQue10();
             obterArrayDeIndexParVezes2MaioresQue9();
         });
 
