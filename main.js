8cd32d61f4c32745b7a5b6eabacc0b2a4df5ca88
Nayana Loivos <loivosdev@gmail.com>
2022-02-28 21:07:10 -0300

    Obtendo array com os números invertidos do cartão

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 
-
-function exibirDigitosDoCartao () {
     let digitosDoCartao = document.querySelector('#boxInputDigitosCartao');
-    let valorDigitosDoCartao = digitosDoCartao.value;
+    let arrayInversaDigitosDoCartao = [];
 
-    let exibirNumeroDoCartao = document.querySelector('#exibirONumeroDoCartao');
-    exibirNumeroDoCartao.textContent = valorDigitosDoCartao;
-}
-   let validarDigitosDoCartao = document.querySelector('#validarNumeroDocartao');
-    validarDigitosDoCartao.addEventListener('click', exibirDigitosDoCartao);
+    function obterArrayInversaDoNumeroDoCartao ( ){
+
+        for(let i = digitosDoCartao.value.length - 1; i >= 0; i--){
+            arrayInversaDigitosDoCartao.push(digitosDoCartao.value[i]);
+        }
+        console.log(arrayInversaDigitosDoCartao); //testar
+    }
+
+    function exibirDigitosDoCartao () {
+        let valorDigitosDoCartao = digitosDoCartao.value;
+
+        let exibirNumeroDoCartao = document.querySelector('#exibirONumeroDoCartao');
+        exibirNumeroDoCartao.textContent = valorDigitosDoCartao;
+    }
+
+       let validarDigitosDoCartao = document.querySelector('#validarNumeroDocartao');
+
+        validarDigitosDoCartao.addEventListener('click',function () {
+            obterArrayInversaDoNumeroDoCartao();
+            exibirDigitosDoCartao();
+        })
 
 
