93fe215e73390f0e5c3eb65d3146d54c3ddc0df0
Nayana Loivos <loivosdev@gmail.com>
2022-03-01 14:49:09 -0300

    Obtendo array de numeros no index par vezes 2

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
 
     let digitosDoCartao = document.querySelector('#boxInputDigitosCartao');
     let arrayInversaDigitosDoCartao = [];
-
-    function obterArrayInversaDoNumeroDoCartao ( ){
-
-        for(let i = digitosDoCartao.value.length - 1; i >= 0; i--){
-            arrayInversaDigitosDoCartao.push(digitosDoCartao.value[i]);
-        }
-        console.log(arrayInversaDigitosDoCartao); //testar
-    }
+    let arrayPosicaoParDaArrayInversaDigitosDoCartao = [];
+   
 
     function exibirDigitosDoCartao () {
         let valorDigitosDoCartao = digitosDoCartao.value;
@@ -17,11 +11,39 @@
         exibirNumeroDoCartao.textContent = valorDigitosDoCartao;
     }
 
-       let validarDigitosDoCartao = document.querySelector('#validarNumeroDocartao');
+    function obterArrayInversaDoNumeroDoCartao (){
+
+        for(let i = digitosDoCartao.value.length - 1; i >= 0; i--){
+            arrayInversaDigitosDoCartao.push(digitosDoCartao.value[i]);
+        }
+        console.log(arrayInversaDigitosDoCartao); //testar
+    }
+    function obterArrayIndexParVezes2 () {
+        for(let i = 0; i < arrayInversaDigitosDoCartao.length; i++) {
+            if( i % 2 === 0 && i !== 0) {
+                let posicao = arrayInversaDigitosDoCartao[i];
+                arrayPosicaoParDaArrayInversaDigitosDoCartao.push(posicao*(2));
+            }
+        }
+    }
+    console.log(arrayPosicaoParDaArrayInversaDigitosDoCartao); //testar
+
+
+
+    let validarDigitosDoCartao = document.querySelector('#validarNumeroDocartao');
 
         validarDigitosDoCartao.addEventListener('click',function () {
-            obterArrayInversaDoNumeroDoCartao();
             exibirDigitosDoCartao();
-        })
+            obterArrayInversaDoNumeroDoCartao();
+            obterArrayIndexParVezes2();
+        });
+
+
+
+
+
+
+
+
 
 
